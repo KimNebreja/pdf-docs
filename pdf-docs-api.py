0f983f0e41b19aeeade946f40b2276db16e6260f
KimNebreja <nebreja.321612@calamba.sti.edu.ph>
2025-03-09 22:57:46 +0800

    Update pdf-docs-api.py

--- pdf-docs-api.py
+++ pdf-docs-api.py
@@ -39,4 +39,4 @@ def convert_pdf_to_docx():
         return f"Conversion error: {str(e)}", 500
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=8000, debug=True)
+    app.run(host='0.0.0.0', port=10000, debug=True)
